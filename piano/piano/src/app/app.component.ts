4340cb44c501381773a3838c6517ce5defea8d28
Harol <hchaverra26@gmail.com>
2024-09-06 18:58:16 -0500

    chore: Add initial project files and configuration

---
+++ piano/piano/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'piano';
+
+
+  public PlaySound(sound: string){
+    console.log("Play sound");
+    let audio = new Audio();
+    audio.src = `../assets/sounds/${sound}.wav` ;
+    audio.load();
+    audio.play();
+
+
+  }
+}
+
+
